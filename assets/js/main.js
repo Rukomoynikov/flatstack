348c113b05017f1f34c3a863cb4bb9b7d39a1d89
Maksim Rukomoynikov <cat@mos-it.com>
2015-09-07 14:17:56 +0300

    Animation for radial bars

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,27 @@ function listener (){
 		navigationPanel.classList.remove("header__nav--fixed")
 	}
 }
+
+// Make radial diagrams
+var diagrams = document.querySelectorAll('.oneSkill');
+Object.prototype.forEach = Array.prototype.forEach;
+
+diagrams.forEach(function(el){
+	var svg = el.childNodes[0];
+	var percent = parseInt(el.childNodes[1].innerText);
+	var bar = el.childNodes[0].childNodes[3];
+	var bar_background = el.childNodes[0].childNodes[1];
+	var radius = svg.height.baseVal.value / 2;
+	el.circleLength = (Math.PI *  radius ) * 2;
+	var bar_offset = "-" + ((100 - percent) / 100 ) * el.circleLength;;
+	bar.style.strokeDasharray = bar_background.style.strokeDasharray = el.circleLength;
+	bar.style.strokeDashoffset = bar_offset;
+	
+	// Хак, для того чтобы начало бара было там где должно быть, иначе начинается справа на отметке 90%.
+	var required_rotate = 275 + Number(bar_offset);
+	svg.style.transform = "rotateZ(" + required_rotate + "deg)";
+})
+
+// Form validation
+var form = document.querySelector('form');
+form.addEventListener('submit');
